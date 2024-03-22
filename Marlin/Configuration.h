4dbce1aead18c6c31d32d8c0cd959e850f702065
Ramon de Ruiter <rderuiter@slashwhy.de>
2024-03-22 10:15:29 +0100

    Use 5x5 probing grid. Disable heating before leveling.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1902,9 +1902,9 @@ FAN2	PB15	Hotend Heat Sink Fan, to cool down the hot end
 /**
  * Auto-leveling needs preheating
  */
-#define PREHEAT_BEFORE_LEVELING
+// #define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
-  #define LEVELING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
+  // #define LEVELING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
   #define LEVELING_BED_TEMP     50
 #endif
 
@@ -1963,7 +1963,7 @@ FAN2	PB15	Hotend Heat Sink Fan, to cool down the hot end
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 7
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
